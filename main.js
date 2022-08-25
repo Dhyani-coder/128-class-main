f9d328b070a335de13f6953135b40d8417abac38
Dhyani-coder <97533534+Dhyani-coder@users.noreply.github.com>
2022-08-25 19:47:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+
+}
+function setup() {
+
+    my_canvas = createCanvas(350, 350);
+
+    background("white");
+    my_canvas.position(800,300);
+
+}
+
+function draw() {
+
+}
